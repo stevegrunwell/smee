a56b51eb9efe3cc29d89714215f6dd484f54fad9
Steve Grunwell <stevegrunwell@gmail.com>
2017-09-16 15:31:22 -0400

    Introduce the smee:install console command

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Smee\Console;
+
+use Exception;
+use Smee\Project;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class InstallCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('smee:install')
+            ->setDescription('Install git hooks for the current project.')
+            ->addOption(
+                'base-dir',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The path to the root directory of the project',
+                getcwd()
+            )
+            ->addOption(
+                'hooks',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The path to the hooks directory, relative to the project root',
+                '.githooks'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $project = new Project($input->getOption('base-dir'), $input->getOption('hooks'));
+        $io = new SymfonyStyle($input, $output);
+
+        try {
+            $copied = $project->copyHooks();
+
+        } catch (Exception $e) {
+            $io->getErrorStyle()->error(sprintf('An error occurred while copying git hooks: %s', $e->getMessage()));
+            return 1;
+        }
+
+        $io->section('Copied git hooks:');
+        $io->listing($copied);
+    }
+}
---
+++ tests/Console/InstallCommandTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Smee\Tests\Console;
+
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+use Smee\Console\InstallCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class InstallCommandTest extends TestCase
+{
+    /**
+     * @var Symfony\Component\Console\Application
+     */
+    protected $app;
+
+    /**
+     * @var Smee\Console\InstallCommand
+     */
+    protected $command;
+
+    /**
+     * @var Symfony\Component\Console\Tester\CommandTester
+     */
+    protected $commandTester;
+
+    /**
+     * @var org\bovigo\vfs\vfsStreamDirectory
+     */
+    protected $root;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->root = vfsStream::setup('project');
+
+        /*
+         * Create a shared instance of the application, along with a reference to the
+         * 'smee:install' command.
+         */
+        $this->app = new Application;
+        $this->app->add(new InstallCommand);
+
+        $this->command = $this->app->find('smee:install');
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    public function testExecute()
+    {
+        vfsStream::create([
+            '.git' => [
+                'hooks' => [],
+            ],
+            '.githooks' => [
+                'pre_commit' => 'pre_commit content',
+                'post_commit' => 'post_commit content',
+            ],
+        ]);
+
+        $this->commandTester->execute([
+            'command' => $this->command->getName(),
+            '--base-dir' => $this->root->url(),
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+
+        $this->assertContains('pre_commit', $output, 'Expected to see "pre_commit" listed in the command output.');
+        $this->assertContains('post_commit', $output, 'Expected to see "post_commit" listed in the command output.');
+    }
+
+    public function testExecuteHandlesCustomHooksDirectory()
+    {
+        vfsStream::create([
+            '.git' => [
+                'hooks' => [],
+            ],
+            'my-githooks-directory' => [
+                'pre_commit' => 'pre_commit content',
+            ],
+        ]);
+
+        $this->commandTester->execute([
+            'command' => $this->command->getName(),
+            '--base-dir' => $this->root->url(),
+            '--hooks' => 'my-githooks-directory',
+        ]);
+
+        $this->assertContains('pre_commit', $this->commandTester->getDisplay());
+    }
+
+    public function testExecuteHandlesMissingGitDirectory()
+    {
+        vfsStream::create([
+            '.githooks' => [
+                'pre_commit' => 'pre_commit content',
+            ],
+        ]);
+
+        $this->assertEquals(1, $this->commandTester->execute([
+            'command' => $this->command->getName(),
+            '--base-dir' => $this->root->url(),
+        ]), 'Command failure should result in an exit code of 1.');
+    }
+
+    public function testExecuteHandlesMissingHooksDirectory()
+    {
+        vfsStream::create([
+            '.git' => [
+                'hooks' => [],
+            ],
+        ]);
+
+        $this->assertEquals(1, $this->commandTester->execute([
+            'command' => $this->command->getName(),
+            '--base-dir' => $this->root->url(),
+        ]), 'Command failure should result in an exit code of 1.');
+    }
+}
